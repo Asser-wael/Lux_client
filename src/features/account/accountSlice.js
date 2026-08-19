fa6fa59c17dc13994975c908da26784ee6b6819f
Asser-wael <aserweal@gmail.com>
2026-08-19 18:01:33 +0300

    update client

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axiosInstance from "../../utils/axiosInstance";
+import axiosInstance from "../../api/axiosInstance";
 
 export const fetchProfile = createAsyncThunk(
   "account/fetchProfile",
