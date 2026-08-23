716ede2efdeef72d5e7774a8cb12538f4cd518b4
Asser-wael <aserweal@gmail.com>
2026-08-23 22:19:58 +0300

    update client

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,24 +1,22 @@
-// hooks/useSocket.js
-import { io } from "socket.io-client";
-import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
-export default function useSocket() {
-  const accessToken = useSelector((state) => state.auth.accessToken);
+const useSocket = () => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(import.meta.env.VITE_API_URL, {
+    const socketInstance = io(import.meta.env.VITE_SOCKET_URL, {
       withCredentials: true,
-      auth: {
-        token: accessToken, // ✅ ده اللي io.use() هيقراه
-      },
     });
 
-    setSocket(newSocket);
+    setSocket(socketInstance);
 
-    return () => newSocket.disconnect();
-  }, [accessToken]);
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
 
   return socket;
-}
+};
+
+export default useSocket;
